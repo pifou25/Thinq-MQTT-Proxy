0e51d9f1f8c22d921ae770c3e8e4f6b25c2b1108
Michal Wojcik <32574975+mgwoj@users.noreply.github.com>
2021-04-12 20:19:06 +0200

    Removed duplicated code in devices

--- drivers/ThinQ_Fridge.groovy
+++ drivers/ThinQ_Fridge.groovy
@@ -34,75 +34,6 @@ String DEFAULT_LOG_LEVEL = LOG_LEVELS[2]
 //    }
 //}
 
-def uninstalled() {
-    logger("debug", "uninstalled()")
-    parent.stopRTIMonitoring(device)
-}
-
-def initialize() {
-    logger("debug", "initialize()")
-    if (getDataValue("master") == "true") {
-        if (interfaces.mqtt.isConnected())
-            interfaces.mqtt.disconnect()
-
-        mqttConnectUntilSuccessful()
-    }
-
-    parent.registerRTIMonitoring(device)
-}
-
-def mqttConnectUntilSuccessful() {
-  logger("debug", "mqttConnectUntilSuccessful()")
-
-  try {
-    def mqtt = parent.retrieveMqttDetails()
-
-    interfaces.mqtt.connect(mqtt.server,
-                            mqtt.clientId,
-                            null,
-                            null,
-                            tlsVersion: "1.2",
-                            privateKey: mqtt.privateKey,
-                            caCertificate: mqtt.caCertificate,
-                            clientCertificate: mqtt.certificate,
-                            ignoreSSLIssues: true)
-    pauseExecution(3000)
-    for (sub in mqtt.subscriptions) {
-        interfaces.mqtt.subscribe(sub, 0, this)
-    }
-    return true
-  }
-  catch (e)
-  {
-    logger("warn", "Lost connection to MQTT, retrying in 15 seconds ${e}")
-    runIn(15, "mqttConnectUntilSuccessful")
-    return false
-  }
-}
-
-def parse(message) {
-    def topic = interfaces.mqtt.parseMessage(message)
-    def payload = new JsonSlurper().parseText(topic.payload)
-    logger("info", "parse(${payload})")
-
-    parent.processMqttMessage(this, payload)
-}
-
-def mqttClientStatus(String message) {
-    logger("debug", "mqttClientStatus(${message})")
-
-    if (message.startsWith("Error:")) {
-        logger("error", "MQTT Error: ${message}")
-
-        try {
-            interfaces.mqtt.disconnect() // Guarantee we're disconnected
-        }
-        catch (e) {
-        }
-        mqttConnectUntilSuccessful()
-    }
-}
-
 def processStateData(data) {
     logger("debug", "processStateData(${data})")
 
@@ -152,20 +83,4 @@ def processStateData(data) {
     }
 }
 
-/**
-* @param level Level to log at, see LOG_LEVELS for options
-* @param msg Message to log
-*/
-private logger(level, msg) {
-  if (level && msg) {
-    Integer levelIdx = LOG_LEVELS.indexOf(level)
-    Integer setLevelIdx = LOG_LEVELS.indexOf(logLevel)
-    if (setLevelIdx < 0) {
-      setLevelIdx = LOG_LEVELS.indexOf(DEFAULT_LOG_LEVEL)
-    }
-    if (levelIdx <= setLevelIdx) {
-      log."${level}" "${device.displayName} ${msg}"
-    }
-  }
-}
 }
